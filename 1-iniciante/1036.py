a0fc33ad1aae07f263a3190b169932779116ec5c
Falcão <gabriel.p.falcao50@gmail.com>
2023-04-22 15:23:44 -0400

    otimização do 1036

--- 1-iniciante/1036.py
+++ 1-iniciante/1036.py
@@ -1,14 +1,15 @@
-from math import sqrt
-line = [float(x) for x in input().split(' ')]
+import math
+import sys
+line = map(float, sys.stdin.readline().split())
 
 a, b, c = line
 
 delta = b**2-4*a*c
 
 if delta < 0 or a == 0:
-    print('Impossivel calcular')
+    sys.stdout.write('Impossivel calcular\n')
 else:
-    r1 = (-b+sqrt(delta))/(2*a)
-    r2 = (-b-sqrt(delta))/(2*a)
-    print(f"R1 = {r1:.5f}")
-    print(f"R2 = {r2:.5f}")
+    r1 = (-b+math.sqrt(delta))/(2*a)
+    r2 = (-b-math.sqrt(delta))/(2*a)
+    sys.stdout.write(f"R1 = {r1:.5f}\n")
+    sys.stdout.write(f"R2 = {r2:.5f}\n")
