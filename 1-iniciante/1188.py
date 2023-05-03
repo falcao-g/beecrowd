a7655f904db5d353353f153488e60fe424b43eba
Falcão <gabriel.p.falcao50@gmail.com>
2023-05-02 22:50:07 -0400

    1188

---
+++ 1-iniciante/1188.py
@@ -0,0 +1,20 @@
+operation = input()
+
+matriz = []
+for i in range(12):
+    matriz.append([])
+    for j in range(12):
+        matriz[i].append(float(input()))
+
+soma = 0
+total = 0
+for i in range(6, 12):
+    for j in range(12):
+        if j > 11-i and j < i:
+            soma += matriz[i][j]
+            total += 1
+
+if operation == 'S':
+    print(f'{soma:.1f}')
+else:
+    print(f'{soma / total:.1f}')
