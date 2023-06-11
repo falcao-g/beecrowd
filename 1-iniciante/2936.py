96dfac8b352b7d1112dbe91ad9a70135d515e56c
Falcão <gabriel.p.falcao50@gmail.com>
2023-06-11 16:07:13 -0400

    2936

---
+++ 1-iniciante/2936.py
@@ -0,0 +1,8 @@
+curupira = int(input())
+boi = int(input())
+boto = int(input())
+mapi = int(input())
+iara = int(input())
+
+total = curupira * 300 + boi * 1500 + boto * 600 + mapi * 1000 + iara * 150 + 225
+print(total)
