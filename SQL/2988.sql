a838c9218e62765176d1e47663152e71106f14e7
Falcão <gabriel.p.falcao50@gmail.com>
2023-09-09 15:25:05 -0400

    2988.sql

---
+++ SQL/2988.sql
@@ -0,0 +1,16 @@
+SELECT
+  t.name AS name,
+  COUNT(m.id) AS matches,
+  SUM(CASE WHEN m.team_1 = t.id AND m.team_1_goals > m.team_2_goals THEN 1
+           WHEN m.team_2 = t.id AND m.team_2_goals > m.team_1_goals THEN 1 ELSE 0 END) AS victories,
+  SUM(CASE WHEN m.team_1 = t.id AND m.team_1_goals < m.team_2_goals THEN 1
+           WHEN m.team_2 = t.id AND m.team_2_goals < m.team_1_goals THEN 1 ELSE 0 END) AS defeats,
+  SUM(CASE WHEN m.team_1_goals = m.team_2_goals THEN 1 ELSE 0 END) AS draws,
+  (SUM(CASE WHEN m.team_1 = t.id AND m.team_1_goals > m.team_2_goals THEN 3
+            WHEN m.team_2 = t.id AND m.team_2_goals > m.team_1_goals THEN 3
+            WHEN m.team_1 = t.id AND m.team_1_goals = m.team_2_goals THEN 1
+            WHEN m.team_2 = t.id AND m.team_2_goals = m.team_1_goals THEN 1 ELSE 0 END)) AS score
+FROM teams t
+LEFT JOIN matches m ON t.id = m.team_1 OR t.id = m.team_2
+GROUP BY t.id, t.name
+ORDER BY score DESC
